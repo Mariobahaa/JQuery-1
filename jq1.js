50586307c51e64fbffe21259e2121e94e9e24e42
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-09 16:55:34 +0200

    Button 2 Working

--- jq1.js
+++ jq1.js
@@ -3,3 +3,14 @@ $("#sasm").hide();
 $("#gal").hide();
 
 $("#bt1").click( function(){$("#sasm").fadeIn(1000).show();});
+
+$("#bt2").click( function(){$("#gal").fadeIn(600).show();});
+
+
+var active = 1;
+
+$("#left").click( function(){active--; console.log(active+".jpg") ;if (active ==0){ active = 1;}
+                            $("#shown").attr("src",active+".jpg");});
+
+$("#right").click( function(){active++; console.log(active+".jpg") ;if(active>=9) {active = 8; }
+                            $("#shown").attr("src",active+".jpg");});
