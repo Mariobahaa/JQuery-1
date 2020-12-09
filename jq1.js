4e547aa6a33b9c16cc5059eb36e46e84f510fd21
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-09 17:06:49 +0200

    Button 3 Update 1

--- jq1.js
+++ jq1.js
@@ -1,16 +1,36 @@
-$("#sasm").hide();
+function hideAll() {
+    $("#sasm").hide();
 
-$("#gal").hide();
+    $("#gal").hide();
+}
 
-$("#bt1").click( function(){$("#sasm").fadeIn(1000).show();});
+hideAll();
 
-$("#bt2").click( function(){$("#gal").fadeIn(600).show();});
+$("#bt1").click(function () { hideAll();
+    $("#sasm").fadeIn(1000).show();
+});
+
+$("#bt2").click(function () { hideAll();
+    $("#gal").fadeIn(600).show();
+});
 
 
 var active = 1;
 
-$("#left").click( function(){active--; console.log(active+".jpg") ;if (active ==0){ active = 1;}
-                            $("#shown").attr("src",active+".jpg");});
+$("#left").click(function () {
+    active--;
+    console.log(active + ".jpg");
+    if (active == 0) {
+        active = 1;
+    }
+    $("#shown").attr("src", active + ".jpg");
+});
 
-$("#right").click( function(){active++; console.log(active+".jpg") ;if(active>=9) {active = 8; }
-                            $("#shown").attr("src",active+".jpg");});
+$("#right").click(function () {
+    active++;
+    console.log(active + ".jpg");
+    if (active >= 9) {
+        active = 8;
+    }
+    $("#shown").attr("src", active + ".jpg");
+});
