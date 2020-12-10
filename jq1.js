8501b7426726dc474739c43aed4b62b3a55be399
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-10 14:16:55 +0200

    Button 4 Completed

--- jq1.js
+++ jq1.js
@@ -2,23 +2,28 @@ function hideAll() {
     $("#sasm").hide();
 
     $("#gal").hide();
-    
+
     $("#opt").hide();
-    
+
     $("#sub").hide();
+    
+    $("#sube").hide();
 }
 
 hideAll();
 
-$("#bt1").click(function () { hideAll();
+$("#bt1").click(function () {
+    hideAll();
     $("#sasm").fadeIn(1000).show();
 });
 
-$("#bt2").click(function () { hideAll();
+$("#bt2").click(function () {
+    hideAll();
     $("#gal").fadeIn(600).show();
 });
 
-$("#bt4").click(function () { hideAll();
+$("#bt4").click(function () {
+    hideAll();
     $("#sub").fadeIn(600).show();
 });
 var active = 1;
@@ -45,10 +50,18 @@ var uname;
 var pass;
 var age;
 var comp;
-$("#send").click(function(){
-    uname = $("#uname").val(); console.log(uname);
-    //debugger;
-    pass = $("#pass").html();
-    age = $("#age").html();
-    comp = $("#comp").html();
+$("#send").click(function () {
+    uname = $("#uname").val(); $("#uname").val('');
+    pass = $("#pass").val(); $("#pass").val('');
+    age = $("#age").val();  $("#age").val('');
+    comp = $("#comp").val(); $("#comp").val('');
+    $("#sub").fadeOut(1200).hide();
+    $("#sube").fadeIn(800).show();
+    $("#sube").css("text-align","left");    
+    //console.log(uname);
+    $("#sube").children()[0].append(uname);
+    console.log(age);
+    $("#sube").children()[5].append(age);
+    console.log(comp);
+    $("#sube").children()[10].append(comp);
 });
